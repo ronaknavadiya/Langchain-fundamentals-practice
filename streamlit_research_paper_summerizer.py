3000d6aceaf1dd51d15d352569eebf404ee1f783
ronak-navadiya <ronaknavadiya123@gmail.com>
2025-08-17 12:18:35 -0400

    chain impl

--- streamlit_research_paper_summerizer.py
+++ streamlit_research_paper_summerizer.py
@@ -26,7 +26,13 @@ prompt = template.invoke({
 })
 
 if st.button("Summarize"):
-    result = model.invoke(prompt)
+    chain = template | model
+    result = chain.invoke({
+        "paper_input":paper_input,
+        "style_input":style_input,
+        "length_input":length_input
+    })
+     
     st.write(result.content)
 
 
